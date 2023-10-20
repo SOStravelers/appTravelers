6e264ad69755992f20f4ee276974b32c0dec92b5
JohannSch <jschacosta@gmail.com>
2023-10-20 15:46:23 +0200

    secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,23 +5,18 @@ import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId:
-        "85474741679-0a1g2k2caj8d59iomohvo2ag6rqr9vlg.apps.googleusercontent.com",
-      clientSecret: "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",
-      scope: [
-        "https://www.googleapis.com/auth/userinfo.profile",
-        "https://www.googleapis.com/auth/userinfo.email",
-      ],
-    }),
+    // GoogleProvider({
+    //   clientId:
+    //     "85474741679-0a1g2k2caj8d59iomohvo2ag6rqr9vlg.apps.googleusercontent.com",
+    //   clientSecret: "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",
+    //   scope: [
+    //     "https://www.googleapis.com/auth/userinfo.profile",
+    //     "https://www.googleapis.com/auth/userinfo.email",
+    //   ],
+    // }),
     FacebookProvider({
       clientId: "707776344534781",
       clientSecret: "f1c9b9607d40ea9a3b67a3d2f3d01385",
     }),
-    // Sign in with passwordless email link
-    // EmailProvider({
-    //   server: process.env.MAIL_SERVER,
-    //   from: "<no-reply@example.com>",
-    // }),
   ],
 });

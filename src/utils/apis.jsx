ca9d7d96317a6cdc283ba510b9a324bcd39c5bd9
JohannSch <jschacosta@gmail.com>
2023-10-22 20:47:45 +0200

    cambios

--- src/utils/apis.jsx
+++ src/utils/apis.jsx
@@ -4,12 +4,14 @@ function SetLocalStorage(env) {
   apis.set("local", "http://localhost:4000/");
   apis.set("dev", "https://www.apidev.sostvl.com/");
   apis.set("development", "https://www.apidev.sostvl.com/");
+  apis.set("production", "https://www.apidev.sostvl.com/");
   apis.set("extra", "https://www.apidev.sostvl.com/");
   const fronts = new Map();
   fronts.set("local", "http://localhost:3000/");
   fronts.set("dev", "https://dev.sostvl.com/");
   fronts.set("development", "https://dev.sostvl.com/");
   fronts.set("extra", "https://app-travelers.vercel.app/");
+  fronts.set("production", "https://app-travelers.vercel.app/");
   const api = apis.get(env.trim());
   const front = fronts.get(env.trim());
   return { api, front };

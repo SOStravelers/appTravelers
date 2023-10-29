e64bbdd95ac1ad92d4750f6c7647a7801b417444
RobertoVRz <roberto.vzrz@gmail.com>
2023-10-29 11:37:45 -0600

    deat:SERvicos workers

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -11,3 +11,23 @@ export const WEEK_DAYS = [
   { id: 6, name: "Saturday" },
   { id: 7, name: "Sunday" },
 ];
+
+export const SERVICES_OPTIONS = [
+  {
+    servicio: "Servicios de Peluquería",
+    subservicios: [
+      { id: 1, name: "Corte de pelo caballero" },
+      { id: 2, name: "Corte de pelo dama" },
+      { id: 3, name: "Corte de pelo niños" },
+      { id: 4, name: "Corte de barba" },
+    ],
+  },
+
+  {
+    servicio: "Servicios de Manicura",
+    subservicios: [
+      { id: 5, name: "Manicura" },
+      { id: 6, name: "Pedicura" },
+    ],
+  },
+];

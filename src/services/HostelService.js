1897bc6e37268f0f2654dc1e19e6428a4901ec00
JohannSch <jschacosta@gmail.com>
2023-11-05 20:19:21 +0100

    cambio url hostels

--- src/services/HostelService.js
+++ src/services/HostelService.js
@@ -3,14 +3,19 @@ import { useStore } from "../store/index";
 
 export default class HostelService {
   static resource = "users";
+  static resourceAuth = "auth";
   static get baseUrl() {
     const { api } = useStore.getState().urls;
-    return `${api}${HostelService.resource}`;
+    return `${api}${UserService.resource}`;
+  }
+  static get authUrl() {
+    const { api } = useStore.getState().urls;
+    return `${api}${UserService.resourceAuth}`;
   }
 
   static async list() {
     let query = "isActive=true&page=1&type=business";
-    return axios.get(`${this.baseUrl}/all?${query}`);
+    return axios.get(`${this.authUrl}/all?${query}`);
   }
   static async get(id) {
     return axios.get(`${this.baseUrl}/${id}`);

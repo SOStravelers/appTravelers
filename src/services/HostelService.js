f0a3f5f08dfcfc9c7d8864abce458aa68b27a44a
JohannSch <jschacosta@gmail.com>
2023-11-05 20:25:15 +0100

    cambio chico

--- src/services/HostelService.js
+++ src/services/HostelService.js
@@ -15,9 +15,9 @@ export default class HostelService {
 
   static async list() {
     let query = "isActive=true&page=1&type=business";
-    return axios.get(`${this.baseUrl}/all?${query}`);
+    return axios.get(`${this.authUrl}/all?${query}`);
   }
   static async get(id) {
-    return axios.get(`${this.baseUrl}/${id}`);
+    return axios.get(`${this.authUrl}/${id}`);
   }
 }

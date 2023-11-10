032bb0c5c37e4fa2dbedf2960f883703678ef878
JohannSch <jschacosta@gmail.com>
2023-11-10 19:08:36 +0100

    flujo calendario vacaciones front listo

--- src/components/mySchedules/SectionCalendar.jsx
+++ src/components/mySchedules/SectionCalendar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import CalendarSchedule from "@/components/utils/calendar/CalendarSchedule";
+import { toast } from "react-toastify";
 
 function SectionCalendar() {
   console.log("inicio");
@@ -9,12 +10,29 @@ function SectionCalendar() {
     console.log("seleccionado");
     setSelectedDate(date);
   }
+  function saveData(data) {
+    console.log("wenazo", data);
+    // let schedules = { schedules: horario };
+    // const response = await ScheduleService.save(schedules);
+    try {
+      toast.info("Saved", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 1200,
+      });
+    } catch (err) {
+      toast.error("Save failed.", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 1500,
+      });
+    }
+  }
 
   return (
     <div className="md:ml-28">
       <CalendarSchedule
         selectedDate={selectedDate}
         onDateChange={handleDateChange}
+        saveData={saveData}
       />
     </div>
   );
--- src/components/utils/calendar/CalendarSchedule.jsx
+++ src/components/utils/calendar/CalendarSchedule.jsx
@@ -5,7 +5,7 @@ import moment from "moment";
 import ScheduleCardCalendar from "../cards/ScheduleCardCalendar";
 import "react-day-picker/dist/style.css";
 
-function CalendarSchedule() {
+function CalendarSchedule({ saveData }) {
   const [disabledDays, setDisabledDays] = useState([]);
   const [range, setRange] = useState(undefined);
   const [selected, setSelected] = useState("");
@@ -15,6 +15,9 @@ function CalendarSchedule() {
   useEffect(() => {
     initialize();
   }, []);
+  useEffect(() => {
+    console.log("perro", disabledDays);
+  }, [disabledDays]);
   const initialize = (dateString = "") => {
     const now = moment();
     if (dateString === now.format("YYYY-MM-DD") || selected === "") {
@@ -23,19 +26,52 @@ function CalendarSchedule() {
       setToDate(now.add(6, "months").toDate());
     }
   };
+  const formatDate = (date) => {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  };
 
-  const handleSelection = () => {
-    console.log("seleccion");
-    setDisabledDays([...disabledDays, range]);
+  const handleSelection = async () => {
+    const dateFrom = formatDate(range.from);
+    const dateTo = formatDate(range.to);
+    let totalDays = [...disabledDays];
+    if (totalDays?.length > 0) {
+      var guardado = false;
+      for (let index = 0; index < totalDays.length; index++) {
+        const item = totalDays[index];
+        const itemFrom = formatDate(item.from);
+        const itemTo = formatDate(item.to);
+        if (dateFrom < itemFrom && dateTo > itemTo) {
+          totalDays.splice(index, 1);
+          if (!guardado) {
+            totalDays[index] = range;
+            guardado = true;
+          }
+        } else {
+          totalDays.push(range);
+          break;
+        }
+      }
+      const rangosOrdenados = totalDays
+        .slice()
+        .sort((a, b) => new Date(a.from) - new Date(b.from));
+      setDisabledDays(rangosOrdenados);
+      await saveData(nuevoRango);
+    } else {
+      const nuevoRango = [range];
+      setDisabledDays(nuevoRango);
+      await saveData(nuevoRango);
+    }
     setRange(undefined);
   };
 
-  const handleCancel = (range) => {
+  const handleCancel = async (range) => {
     console.log("cancel");
     const newDisabledDays = disabledDays.filter(
       (day) => day.from !== range.from && day.to !== range.to
     );
+    await saveData(newDisabledDays);
     setDisabledDays(newDisabledDays);
+    console.log("disableDay", disabledDays);
   };
 
   let footer = <p className="my-5 text-center">Please pick a day.</p>;

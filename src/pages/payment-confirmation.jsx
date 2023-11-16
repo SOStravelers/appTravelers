5dab44e9d203b24ba741563a05fe9cb857d3a567
JohannSch <jschacosta@gmail.com>
2023-11-16 17:49:26 +0100

    confirmation

--- src/pages/payment-confirmation.jsx
+++ src/pages/payment-confirmation.jsx
@@ -46,7 +46,7 @@ export default function PaymentConfirmation() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center my-5 px-10 min-h-screen">
+    <div className="flex flex-col items-center justify-center my-4 px-10 min-h-screen">
       <Image src={"/assets/bgPayment.png"} width={250} height={250} />
       <p className="text-blackText text-center mt-10 mb-20 w-3/4 text-lg">
         Payment made and your booking has been made successfully.

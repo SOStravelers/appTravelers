37d76b48bf320aee8d8d1b9730ae91400dba7927
JohannSch <jschacosta@gmail.com>
2023-11-26 11:20:00 +0100

    data user in payment intent

--- src/services/StripeService.js
+++ src/services/StripeService.js
@@ -7,8 +7,17 @@ export default class StripeService {
     const { api } = useStore.getState().urls;
     return `${api}${StripeService.resource}`;
   }
+  static getHeaders() {
+    return {
+      Authorization: localStorage.getItem("auth.access_token")
+        ? localStorage.getItem("auth.access_token")
+        : {},
+    };
+  }
 
   static async createPaymentIntent(params) {
-    return axios.post(`${this.baseUrl}/payment-intents`, params);
+    return axios.post(`${this.baseUrl}/payment-intents`, params, {
+      headers: this.getHeaders(),
+    });
   }
 }

6b80cbdc394ac4df908db63ebc8a7432ef88cb59
RobertoVRz <roberto.vzrz@gmail.com>
2023-11-26 02:25:49 -0600

    Refactor forgot password form and add resend code functionality

--- src/pages/forgot-password.jsx
+++ src/pages/forgot-password.jsx
@@ -2,14 +2,19 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import UserService from "@/services/UserService";
 import ForgotPassForm from "@/components/utils/forms/ForgotPassForm";
+import OutlinedButton from "@/components/utils/buttons/OutlinedButton";
 import OutlinedInput from "@/components/utils/inputs/OutlinedInput";
 import SolidButton from "@/components/utils/buttons/SolidButton";
+import { toast } from "react-toastify";
 
 export default function ChangePassword() {
   const [codeSend, setCodeSend] = useState(false);
   const [code, setCode] = useState("");
+  const [email, setEmail] = useState("");
   const [userId, setUserId] = useState("");
+  const [buttonDisabled, setButtonDisabled] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
+
   const router = useRouter();
   useEffect(() => {
     document.title = "Recovery Pass - SOS Travelers";
@@ -32,6 +37,48 @@ export default function ChangePassword() {
     }
   };
 
+  const handleVerifyMail = async (e) => {
+    setErrorMsg("");
+    e.preventDefault();
+    try {
+      const response = await UserService.findByEmail(email);
+      if (response?.data?.isActive && response?.data?.isValidate) {
+        console.log(response);
+        const res = await UserService.sendCodeEmail(
+          response.data._id,
+          "forgotPass"
+        );
+        if (res.status === 200) {
+          setCodeSend(true);
+          setUserId(response.data._id);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      setErrorMsg(err.response?.data?.message);
+    }
+  };
+
+  const handleResendCode = async () => {
+    try {
+      const response = await UserService.sendCodeEmail(userId, "forgotPass");
+      if (response.status === 200) {
+        startCounter();
+        toast.success("Code Send");
+        setButtonDisabled(true);
+        console.log(response);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const startCounter = () => {
+    setTimeout(() => {
+      setButtonDisabled(false);
+    }, 300000);
+  };
+
   return (
     <div className="px-5 py-28 md:pl-80">
       {codeSend ? (
@@ -46,6 +93,13 @@ export default function ChangePassword() {
           />
           <p className="text-red text-center my-2">{errorMsg}</p>
           <SolidButton mt={5} text="Verify" onClick={handleVerifyCode} />
+          <OutlinedButton
+            secondary
+            text="Resend code"
+            mt={5}
+            onClick={handleResendCode}
+            disabled={buttonDisabled}
+          />
         </div>
       ) : (
         <>
@@ -56,7 +110,11 @@ export default function ChangePassword() {
             Enter your email address below and we&apos;ll send you a code to
             reset your password.
           </p>
-          <ForgotPassForm setCodeSend={setCodeSend} setUserId={setUserId} />
+          <ForgotPassForm
+            errorMsg={errorMsg}
+            handleVerifyMail={handleVerifyMail}
+            setEmail={setEmail}
+          />
         </>
       )}
     </div>

c46726d51c17920e18030fafce76c6cfa9924906
RobertoVRz <roberto.vzrz@gmail.com>
2023-12-16 18:57:27 -0600

    Refactor login flow and add verification page (#18)

---
+++ src/pages/verify-account.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import UserService from "@/services/UserService";
+import OutlinedInput from "@/components/utils/inputs/OutlinedInput";
+import SolidButton from "@/components/utils/buttons/SolidButton";
+
+export default function VerifyAccount() {
+  const [code, setCode] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    document.title = "Recovery Pass - SOS Travelers";
+  }, []);
+  const handleVerifyCode = async () => {
+    try {
+      const userId = router?.query?.userId;
+      const response = await UserService.verifyCodeEmail(userId, code);
+      if (response.status === 200) {
+        router.push({
+          pathname: "/",
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      setErrorMsg(err?.response?.data?.message);
+    }
+  };
+
+  return (
+    <div className="px-5 py-28 md:pl-80">
+      <div className="max-w-lg">
+        <p className="text-center text-gray-500 mb-5">
+          Please check your email for the verification code to validate your
+          account.
+        </p>
+        <OutlinedInput
+          placeholder="Verification code"
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <p className="text-red text-center my-2">{errorMsg}</p>
+        <SolidButton mt={5} text="Verify" onClick={handleVerifyCode} />
+      </div>
+    </div>
+  );
+}

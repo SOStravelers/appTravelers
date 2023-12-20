c3c5182ed17a8713ee070bc5d3c91d59a34c573e
JohannSch <jschacosta@gmail.com>
2023-12-20 20:36:44 -0300

    diseño ajustes

--- src/components/utils/cards/WorkerCardSumary.jsx
+++ src/components/utils/cards/WorkerCardSumary.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { StarIcon, ChangeIcon } from "@/constants/icons";
+import { useState, useEffect } from "react";
 
 function WorkerCardSumary({
   link,
@@ -12,12 +13,31 @@ function WorkerCardSumary({
   showEdit = true,
 }) {
   const router = useRouter();
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    setWidth(window.innerWidth);
 
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   const handleEditWorker = () => {
     const service = JSON.parse(localStorage.getItem("service"));
     localStorage.setItem("editing", true);
     router.push(`/workers-found/${service.service.hostelId}`);
   };
+  function truncate(str, num) {
+    if (str.length <= num) {
+      return str;
+    }
+    return str.slice(0, num) + "...";
+  }
 
   return (
     <div className="flex py-4 w-full max-w-lg rounded-lg justify-between my-2 items-center">
@@ -36,7 +56,9 @@ function WorkerCardSumary({
         </div>
         <div className="flex mt-2 flex-col">
           <h1 className="font-semibold">{name}</h1>
-          <p className="text-blackText text-sm">{service}</p>
+          <p className="text-blackText text-sm">
+            {width < 420 ? truncate(service, 26) : service}
+          </p>
           {/* <div className="flex items-center">
             <StarIcon color={"#00A0D5"} className="mr-1" />
             <p className="text-blackText">{score}</p>

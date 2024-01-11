086ba821a538ead9ebde48185808e0fe53dd077d
Metal <methalcon@gmail.com>
2024-01-11 22:35:39 +0100

    upcoming worker

--- src/pages/worker/home.jsx
+++ src/pages/worker/home.jsx
@@ -3,7 +3,11 @@ import { useRouter } from "next/router";
 import { Swiper, SwiperSlide } from "swiper/react";
 import BookingCard from "@/components/utils/cards/BookingCard";
 import OutlinedButton from "@/components/utils/buttons/OutlinedButton";
-import WorkerCardBookingWorker from "@/components/utils/cards/WorkerCardBookingWorker";
+
+import dayjs from "dayjs";
+import BookingService from "@/services/BookingService";
+import WorkerCardBooking from "@/components/utils/cards/WorkerCardBooking";
+import { Rings } from "react-loader-spinner";
 
 import UserService from "@/services/UserService";
 import { useStore } from "@/store";
@@ -15,13 +19,31 @@ import { set } from "date-fns";
 
 export default function WorkerHome() {
   const router = useRouter();
-  const [bookings, setBookings] = useState([]);
+  const [slides, setSlides] = useState([]);
   const { setService, user } = useStore();
   const [swiper, setSwiper] = useState(null);
+  const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getBookings();
+  }, []);
+
+  const getBookings = () => {
+    const today = dayjs().format("YYYY-MM-DD");
+    BookingService.getBookingsByList(today)
+      .then((response) => {
+        setBookings(response.data.docs);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
     document.title = "Worker Home | SOS Travelers";
-    setBookings([
+    setSlides([
       {
         id: 1,
         direction: "124 street Miro Hotel, Ubud",
@@ -74,8 +96,10 @@ export default function WorkerHome() {
     router.push(path);
   };
 
+  console.log(bookings);
+
   return (
-    <main className="flex flex-col bg-white  py-16 lg:mt-5 xl:mt-5 px-4 md:pl-80">
+    <main className="flex flex-col bg-white  py-16 lg:mt-5 xl:mt-5 px-4 md:pl-80 md:items-start">
       <div className="w-full max-w-lg ">
         <Swiper
           spaceBetween={10}
@@ -88,36 +112,62 @@ export default function WorkerHome() {
             "--swiper-pagination-bullet-active-color": "#1111",
           }}
         >
-          {bookings?.map((booking) => (
-            <SwiperSlide key={booking?.id}>
-              <BookingCard
-                direction={booking?.direction}
-                date={booking?.date}
-              />
+          {slides?.map((slide) => (
+            <SwiperSlide key={slide?.id}>
+              <BookingCard direction={slide?.direction} date={slide?.date} />
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
 
-      <section className=" mb-5">
-        <OutlinedButton text="Book a service" onClick={handleBookService} />
+      <section className=" mb-5 flex w-full max-w-lg items-center ">
+        <div className="flex flex-grow items-center ">
+          <OutlinedButton text="Book a service" onClick={handleBookService} />
+        </div>
       </section>
 
-      <section className="flex flex-col items-center md:items-start">
-        <h1 className="text-black text-xl font-medium text-center my-5">
-          Upcoming
-        </h1>
-
-        <WorkerCardBookingWorker
-          link="/worker/booking"
-          name="John Doe"
-          location="New York, USA"
-        />
-        <WorkerCardBookingWorker
-          link="/worker/booking"
-          name="John Doe"
-          location="New York, USA"
-        />
+      <section className="flex w-full max-w-lg items-center flex-col">
+        <div className="items-center ">
+          <h1 className="text-black text-xl font-medium text-center my-5">
+            Upcoming
+          </h1>
+        </div>
+        <div className="flex flex-col w-full max-w-lg">
+          {loading ? (
+            <div className="max-w-lg flex flex-col items-center justify-center">
+              <Rings
+                width={100}
+                height={100}
+                color="#00A0D5"
+                ariaLabel="infinity-spin-loading"
+              />
+            </div>
+          ) : (
+            bookings
+              /*                 .filter(
+                  (booking) =>
+                    booking.date.stringData === dayjs().format("YYYY-MM-DD")
+                ) */
+              .map((booking) => (
+                <WorkerCardBooking
+                  key={booking._id}
+                  booking={booking}
+                  subService={booking.subservice.name}
+                  status={booking.status}
+                  service={booking.service.name}
+                  avatar={booking?.businessUser?.img?.imgUrl}
+                  date={booking.date.stringData}
+                  hour={booking.startTime.stringData}
+                  name={`${booking?.businessUser?.businessData?.name}`}
+                />
+              ))
+          )}
+          {!loading && bookings.length === 0 && (
+            <p className="text-center text-greyText max-w-lg my-10">
+              {"Não há reservas para hoje"}
+            </p>
+          )}
+        </div>
       </section>
     </main>
   );

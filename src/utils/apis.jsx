ed8af53333f350771106a1573cf9368c4435c52b
JohannSch <jschacosta@gmail.com>
2024-01-17 00:23:50 -0300

    ambiente testing

--- src/utils/apis.jsx
+++ src/utils/apis.jsx
@@ -4,13 +4,13 @@ function SetLocalStorage(env) {
   apis.set("local", "http://localhost:9000/");
   apis.set("dev", "https://www.apidev.sostvl.com/");
   apis.set("development", "https://www.apidev.sostvl.com/");
+  apis.set("test", "https://www.apitest.sostvl.com/");
   apis.set("production", "https://www.api.sostvl.com/");
-  apis.set("extra", "https://www.apidev.sostvl.com/");
   const fronts = new Map();
   fronts.set("local", "http://localhost:3000/");
   fronts.set("dev", "https://dev.sostvl.com/");
+  fronts.set("test", "https://test.sostvl.com/");
   fronts.set("development", "https://dev.sostvl.com/");
-  fronts.set("extra", "https://app-travelers.vercel.app/");
   fronts.set("production", "https://sostvl.com/");
   const api = apis.get(env.trim());
 

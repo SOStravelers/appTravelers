a121ee4afd4177f98c34c86ad27216b7e9f2ab3d
JohannSch <jschacosta@gmail.com>
2024-01-31 19:33:17 -0300

    mensajes

--- src/pages/payment-demo.jsx
+++ src/pages/payment-demo.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
-import { CompleteGirlIcon, BarberPicture } from "@/constants/icons";
+import {
+  CompleteGirlIcon,
+  BarberPicture,
+  ArrowUpIcon,
+} from "@/constants/icons";
 import SolidButton from "@/components/utils/buttons/SolidButton";
 import ContactForm from "@/components/utils/forms/ContactForm";
-
+import ComboBox from "@/components/utils/combobox/ComboBox";
 export default function PaymentConfirmation() {
   const router = useRouter();
   const [loading, setLoading] = useState(true);
@@ -11,13 +15,26 @@ export default function PaymentConfirmation() {
   useEffect(() => {
     document.title = "Confirmation | SOS Travelers";
   }, []);
+  const [isTextVisible1, setIsTextVisible1] = useState(false);
+  const [isTextVisible2, setIsTextVisible2] = useState(false);
+  const [isTextVisible3, setIsTextVisible3] = useState(false);
+
+  const toggleText1 = () => {
+    setIsTextVisible1(!isTextVisible1);
+  };
+  const toggleText2 = () => {
+    setIsTextVisible2(!isTextVisible2);
+  };
+  const toggleText3 = () => {
+    setIsTextVisible3(!isTextVisible3);
+  };
   const goTo = () => {
     console.log(booking);
     router.push(`/`);
   };
 
   return (
-    <div className="flex flex-col max-w-lg items-center justify-center my-1 px-3 md:ml-80 min-h-screen md:pt-20">
+    <div className="flex flex-col max-w-lg items-center justify-center my-1 mb-10 px-3 md:ml-80 min-h-screen md:pt-20">
       <div
         className="flex items-center justify-center"
         style={{ marginTop: "-110px" }}
@@ -43,6 +60,115 @@ export default function PaymentConfirmation() {
       <div className="w-80  md:w-80 ">
         <ContactForm />
       </div>
+
+      <div
+        className="text-sm text-black rounded-lg px-2 py-2 my-5 flex justify-between items-center cursor-pointer"
+        onClick={toggleText1}
+      >
+        <h1 className=" text-lg">O que a Sos Travelers faz pelo seu Hostel?</h1>
+        <ArrowUpIcon
+          color={"#5B78C7"}
+          className={`${
+            isTextVisible1 ? "rotate-180" : ""
+          } transform scale-200`}
+        />
+      </div>
+
+      {isTextVisible1 && (
+        <p className="text-left mx-4">
+          <p className=" font-bold"> Variedade de serviços personalizados</p>
+          <p>
+            De massagens relaxantes a cortes de cabelo e tatuagens, conectamos
+            seus hóspedes com uma ampla gama de serviços personalizados para
+            atender às suas necessidades.
+          </p>
+          <p className=" font-bold"> Plataforma fácil de user</p>
+          <p>
+            Nosso aplicativo intuitivo facilita aos seus hóspedes descobrir e
+            reservar serviços com apenas alguns toques em seus dispositivos
+            móveis.
+          </p>
+          <p className=" font-bold"> Valor agregado para seus hóspedes</p>
+          <p>
+            Com a Sos Travelers, seus hóspedes desfrutarão de uma experiência
+            além das comodidades tradicionais, tornando sua estadia algo
+            inesquecível.
+          </p>
+        </p>
+      )}
+
+      <div
+        className="text-sm text-black rounded-lg px-2 py-2 my-5 flex justify-between items-center cursor-pointer"
+        onClick={toggleText2}
+      >
+        <h1 className="text-lg">
+          Como a Sos Travelers beneficia o seu Hostel?
+        </h1>
+        <ArrowUpIcon
+          color={"#5B78C7"}
+          className={`${
+            isTextVisible2 ? "rotate-180" : ""
+          } transform scale-200`}
+        />
+      </div>
+
+      {isTextVisible2 && (
+        <p className="text-left mx-4">
+          <p className=" font-bold"> Difenciação competitiva</p>
+          <p>
+            Destaque-se oferecendo serviços inovadores e únicos que melhorarão a
+            reputação do seu hostel frente à concorrência.
+          </p>
+          <p className=" font-bold"> Geração de receita adicional</p>
+          <p>
+            Obtenha receita extra por meio de comissões sobre transações
+            realizadas em nossa plataforma. Oferecemos 20% de cada serviço
+            realizado.
+          </p>
+          <p className=" font-bold"> Maior retensão de hóspedes</p>
+          <p>
+            Fomentamos a lealdade de seus hóspedes ao proporcionar experiências
+            que superam suas expectativas, aumentando assim a probabilidade de
+            retorno.
+          </p>
+        </p>
+      )}
+
+      <div
+        className="text-sm text-black rounded-lg px-2 py-2 my-5 flex justify-between items-center cursor-pointer"
+        onClick={toggleText3}
+      >
+        <h1 className="text-lg">Como Você Pode Começar?</h1>
+        <ArrowUpIcon
+          color={"#5B78C7"}
+          className={`${
+            isTextVisible3 ? "rotate-180" : ""
+          } transform scale-200`}
+        />
+      </div>
+
+      {isTextVisible3 && (
+        <p className="text-left mx-4">
+          <p className=" font-bold"> Registro Simples</p>
+          <p>
+            Junte-se à Sos Travelers com um simples processo de registro para
+            hostels.
+          </p>
+          <p className=" font-bold"> Personalização de Serviços</p>
+          <p>
+            Escolha os serviços que melhor se adequam às preferências e estilo
+            do seu hostel e fortaleça sua oferta. Aumente a satisfação de seus
+            hóspedes ao oferecer serviços exclusivos e personalizados por meio
+            da Sos Travelers, com controle total 24 horas. Com seu registro,
+            você poderá marcar seus dias e horários disponíveis, ver os serviços
+            agendados, as comissões geradas e obter informações sobre nossos
+            colaboradores.
+          </p>
+        </p>
+      )}
+
+      <p className="text-left mt-2 mb-5 font-bold mx-4">Team Sos travelers</p>
+
       {/* <div className="mx-12 w-50 px-1" onClick={goTo}>
         <SolidButton
           onClick={() => goTo()}

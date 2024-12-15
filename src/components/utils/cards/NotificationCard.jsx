412f267fa04591d32f95e034e31c8e1ba4be6c95
JohannSch <jschacosta@gmail.com>
2024-12-15 15:49:18 -0300

    punto notificacion

--- src/components/utils/cards/NotificationCard.jsx
+++ src/components/utils/cards/NotificationCard.jsx
@@ -16,15 +16,18 @@ function NotificationCard({ id, day, body, title, link, isRead }) {
       href={link}
       className="flex p-2 w-full max-w-lg rounded-2xl border-b-2 border-blueBorder my-1 items-center"
     >
-      <div size={1} className="bg-blueBorder p-2 rounded-full mr-2">
-        <NotificationIcon color="#ffffff" size={1} />
+      <div
+        size={1}
+        className="bg-blueBorder p-2 rounded-full mr-2 flex-shrink-0"
+        style={{ width: "40px", height: "40px" }}
+      >
+        <NotificationIcon color="#ffffff" size={20} />
       </div>
       <div className="flex-grow">
         <p className={` ${!isRead ? "text-black " : "text-greyText"}`}>
           {title}
         </p>
         <p className="text-sm text-blackText">{body}</p>
-
         <p
           className={`text-xs ${
             !isRead ? "text-black font-semibold" : "text-greyText"
@@ -40,7 +43,7 @@ function NotificationCard({ id, day, body, title, link, isRead }) {
             height: "10px",
             width: "10px",
           }}
-          className="rounded-full self-center"
+          className="rounded-full self-center flex-shrink-0"
         ></div>
       )}
     </Link>

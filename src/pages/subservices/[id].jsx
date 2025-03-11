9792d3708f85fdb130bfd84a78449e01b1b097b8
JohannSch <jschacosta@gmail.com>
2025-03-11 16:30:22 -0300

    minicabio

--- src/pages/subservices/[id].jsx
+++ src/pages/subservices/[id].jsx
@@ -14,7 +14,6 @@ export default function Subservices() {
   const [loading, setLoading] = useState(true);
   const [subServices, setSubservices] = useState([]);
 
-  
   const idPaseos = "6757137ad2b2668720116ec9";
 
   useEffect(() => {
@@ -68,7 +67,7 @@ export default function Subservices() {
     if (service.serviceId == "67c11c4917c3a7a2c353cb1b") {
       let urlAmigable = encodeURIComponent(miniService.name[language]);
       //minicambio
-      const url = `https://sostvl.com/summary-custom?date=2025-03-08&workerId=65312a63c0b1e1658a5a712c&subServiceId=${miniService._id}&isoTime=2025-03-15T17:45:00.000Z&stringData=17:45&nameSubservice=${urlAmigable}&price=${miniService.price.category1}`;
+      const url = `https://sostvl.com/summary-custom?date=2025-03-12&workerId=65312a63c0b1e1658a5a712c&subServiceId=${miniService._id}&isoTime=2025-03-12T21:30:00.000Z&stringData=21:30&nameSubservice=${urlAmigable}&price=${miniService.price.category1}`;
       const urlSimple = `/summary-custom?date=2025-03-12&workerId=65312a63c0b1e1658a5a712c&subServiceId=${miniService._id}&isoTime=2025-03-12T21:30:00.000Z&stringData=21:30&nameSubservice=${urlAmigable}&price=${miniService.price.category1}`;
       // router.push(urlSimple);
       window.location.href = url;

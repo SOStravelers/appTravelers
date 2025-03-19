81f623500e90b74059d5e770a792fa759749c224
Sos Travelers <141832403+SOStravelers@users.noreply.github.com>
2025-03-19 20:03:15 -0300

    Update payment-confirmation.jsx

--- src/pages/payment-confirmation.jsx
+++ src/pages/payment-confirmation.jsx
@@ -5,7 +5,6 @@ import { Rings } from "react-loader-spinner";
 import Link from "next/link";
 
 import Cookies from "js-cookie";
-
 //prueba handle 6515996e711d8a6f4596e19e3f0d2e49f59f1f84
 import { CompleteGirlIcon, BarberPicture } from "@/constants/icons";
 import { io } from "socket.io-client";

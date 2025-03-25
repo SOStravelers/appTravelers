903658077e529f4f89686fdf9756bea9eb02c4af
JohannSch <jschacosta@gmail.com>
2025-03-25 01:28:20 -0300

    minicambio routas partidos

--- src/pages/subservices/[id].jsx
+++ src/pages/subservices/[id].jsx
@@ -59,6 +59,12 @@ export default function Subservices() {
       "_blank"
     );
   };
+
+  function obtenerFechaYHora(isoString) {
+    const [fecha, horaCompleta] = isoString.split("T");
+    const hora = horaCompleta.slice(0, 5); // solo HH:mm
+    return { fecha, hora };
+  }
   //minicambio
   const selectSubservice = (miniService) => {
     console.log("erno", miniService);
@@ -66,9 +72,11 @@ export default function Subservices() {
 
     if (service.serviceId == "67c11c4917c3a7a2c353cb1b") {
       let urlAmigable = encodeURIComponent(miniService.name[language]);
+
+      const fechas = obtenerFechaYHora(miniService.isoTime);
       //minicambio2
-      const url = `https://sostvl.com/summary-custom?date=2025-03-16&workerId=65312a63c0b1e1658a5a712c&subServiceId=${miniService._id}&isoTime=2025-03-16T16:00:00.000Z&stringData=16:00&nameSubservice=${urlAmigable}&price=${miniService.price.category1}`;
-      const urlSimple = `/summary-custom?date=2025-03-16&workerId=65312a63c0b1e1658a5a712c&subServiceId=${miniService._id}&isoTime=2025-03-12T21:30:00.000Z&stringData=21:30&nameSubservice=${urlAmigable}&price=${miniService.price.category1}`;
+      const url = `https://sostvl.com/summary-custom?date=${fechas.fecha}&workerId=65312a63c0b1e1658a5a712c&subServiceId=${miniService._id}&${miniService.isoTime}&stringData=${fechas.hora}&nameSubservice=${urlAmigable}&price=${miniService.price.category1}`;
+      const urlSimple = `/summary-custom?date=${fechas.fecha}&workerId=65312a63c0b1e1658a5a712c&subServiceId=${miniService._id}&${miniService.isoTime}&stringData=${fechas.hora}&nameSubservice=${urlAmigable}&price=${miniService.price.category1}`;
       // router.push(urlSimple);
       window.location.href = url;
       return;

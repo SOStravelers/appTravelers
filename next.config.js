2d9fdbbe56b176b0a482740c20188937f1888d6b
JohannSch <jschacosta@gmail.com>
2025-07-06 12:19:51 -0300

    denuevo next.config

--- next.config.js
+++ next.config.js
@@ -19,11 +19,11 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' https://accounts.google.com https://apis.google.com 'unsafe-inline';
-              frame-src https://accounts.google.com;
-              connect-src 'self' https://accounts.google.com https://oauth2.googleapis.com;
-              img-src 'self' https://lh3.googleusercontent.com data:;
-              style-src 'self' 'unsafe-inline';
+              script-src 'self' 'unsafe-inline' https://accounts.google.com https://apis.google.com https://www.googletagmanager.com https://vercel.live https://js.stripe.com;
+              style-src 'self' 'unsafe-inline' https://accounts.google.com https://js.stripe.com;
+              img-src 'self' data: https://lh3.googleusercontent.com;
+              frame-src https://accounts.google.com https://js.stripe.com https://hooks.stripe.com;
+              connect-src 'self' http://localhost:9000 https://accounts.google.com https://oauth2.googleapis.com https://www.google-analytics.com https://vercel.live https://api.stripe.com;
             `
               .replace(/\s{2,}/g, " ")
               .trim(),

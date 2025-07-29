593ce232065dca4e2f2b30c831f5fae7a053eb29
JohannSch <jschacosta@gmail.com>
2025-07-29 00:14:09 -0300

    config

--- src/pages/config/index.jsx
+++ src/pages/config/index.jsx
@@ -30,6 +30,13 @@ export default function Profile({ user }) {
             `}
     >
       <div className="w-full max-w-md flex flex-col self-center">
+        <h1 className="text-center text-textColor text-xl mb-5 ">
+          Admistrador
+        </h1>
+
+        <h2 className=" text-textColor text-md mb-3 mt-5  ml-20">
+          1. Configuraciones
+        </h2>
         <OutlinedButton
           onClick={() => router.push("/config/subservice")}
           text={"Config Subservicios"}
@@ -64,10 +71,23 @@ export default function Profile({ user }) {
           textColor="text-white"
           buttonCenter={true}
         />
-
+        <h2 className=" text-textColor text-md mb-2 mt-8  ml-20">
+          2. Reservas y pagos
+        </h2>
         <OutlinedButton
           onClick={() => router.push("/config/bookings")}
           text={"Agenda de bookings"}
+          px={8}
+          margin={"my-3"}
+          py={2}
+          dark="darkLight"
+          textSize="text-sm"
+          textColor="text-white"
+          buttonCenter={true}
+        />
+        <OutlinedButton
+          onClick={() => router.push("/config/createPaymentLink")}
+          text={"Generar Link Stripe"}
           px={8}
           margin={"my-3"}
           py={2}
